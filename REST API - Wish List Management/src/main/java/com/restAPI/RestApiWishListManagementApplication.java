08bf07710da69971b336ca7dde65a6385a3bedaf
Gerald Gelats <142944007+ggela123@users.noreply.github.com>
2023-11-06 21:04:07 -0500

    Add files via upload

---
+++ REST API - Wish List Management/src/main/java/com/restAPI/RestApiWishListManagementApplication.java
@@ -0,0 +1,14 @@
+package com.restAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiWishListManagementApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(RestApiWishListManagementApplication.class, args);
+    }
+
+}
